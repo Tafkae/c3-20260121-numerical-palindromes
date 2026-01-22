9d6e875b1076acd10114664c02560c34f27a57f4
Lisa H. <neuroticNanobot@outlook.com>
2026-01-21 19:52:40 -0500

    brute-force solution

--- src/kata.js
+++ src/kata.js
@@ -1,7 +1,33 @@
 class Kata {
-    hello(name) {
-        return `Hello ${name}`;
+  hello(name) {
+    return `Hello ${name}`;
+  }
+
+  checkPalindrome(num) {
+    if (!Number.isInteger(num) || num < 0) return false;
+
+    let strNum = num.toString();
+    let rvsNum = strNum.split("").reverse().join("");
+    if (strNum === rvsNum) return true;
+    return false; // default
+  }
+
+  // brute-force solution
+  generatePalindromes(qty) {
+    if (qty < 1) return [];
+
+    let pals = [0];
+    let next = pals.at(-1) + 1;
+
+    while (pals.length < qty) {
+      if (this.checkPalindrome(next)) {
+        pals.push(next);
+      }
+      next ++;
     }
+    console.log(pals);
+    return pals;
+  }
 }
 
 module.exports = Kata;
--- src/kata.spec.js
+++ src/kata.spec.js
@@ -1,14 +1,45 @@
-const Kata = require('./kata');
+const Kata = require("./kata");
 
 let fixture;
 
 // This is a Jest unit test - see https://jestjs.io/docs/en/getting-started for more information
 
 beforeEach(() => {
-    fixture = new Kata();
+  fixture = new Kata();
 });
 
-test('hello should return message with specified name', () => {
-    const name = 'Ingage';
-    expect(fixture.hello(name)).toEqual(`Hello ${name}`);
+test("hello should return message with specified name", () => {
+  const name = "Ingage";
+  expect(fixture.hello(name)).toEqual(`Hello ${name}`);
+});
+
+test("checkPalindrome should be false if input is not a positive integer", () => {
+  expect(fixture.checkPalindrome(-5)).toEqual(false);
+  expect(fixture.checkPalindrome("A")).toEqual(false);
+});
+
+test("checkPalindrome should be true for single digit numbers", () => {
+  expect(fixture.checkPalindrome(0)).toEqual(true);
+  expect(fixture.checkPalindrome(5)).toEqual(true);
+});
+
+test("checkPalindrome should identify numeric palindromes", () => {
+  const samplePals = [0, 5, 11, 232, 4994, 10501];
+  expect(fixture.checkPalindrome(samplePals[0])).toEqual(true);
+  expect(fixture.checkPalindrome(samplePals[1])).toEqual(true);
+  expect(fixture.checkPalindrome(samplePals[2])).toEqual(true);
+  expect(fixture.checkPalindrome(samplePals[3])).toEqual(true);
+  expect(fixture.checkPalindrome(samplePals[4])).toEqual(true);
+  expect(fixture.checkPalindrome(samplePals[5])).toEqual(true);
+});
+
+test("generatePalindromes should generate the first x palindrome numbers", () => {
+  expect(fixture.generatePalindromes(10)).toEqual([
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+  ]);
+  expect(fixture.generatePalindromes(19)).toEqual([
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22, 33, 44, 55, 66, 77, 88, 99
+  ]);
+
+  fixture.generatePalindromes(10000);
 });
